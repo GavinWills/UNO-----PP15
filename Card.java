0f71fad756b6b00a985f4fa11e72dfdb86fcdd72
GavinWills <48930613+GavinWills@users.noreply.github.com>
2019-03-26 15:33:29 -0700

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,14 @@
+public class Card{
+  private String color, value, type;
+  public Card (String col, String val, String ty){
+    color = col;
+    value = val;
+    type = ty; 
+  }
+  public boolean Com(Card a, Card DisCard){
+    if (
+  }
+  public toString(){
+    return color + " " + value + " " + type;
+  }
+}
